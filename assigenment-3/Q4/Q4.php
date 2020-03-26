e46821da5ffd47d9f71b79db158d0f3287ebf635
Monishwaran v <42244770+Monishwaranvenkat@users.noreply.github.com>
2020-03-26 20:56:39 +0530

    Q4

--- assigenment-3/Q4/Q4.php
+++ assigenment-3/Q4/Q4.php
@@ -1,11 +1,11 @@
-<html>
-<body>
-<form method="post">
-Enter First Number:
-<input type="number" name="number1" /><br><br>
-Enter Second Number:
-<input type="number" name="number2" /><br><br>
-<input type="submit" name="submit" value="Add">
+<html> 
+<body> 
+<form method="post"> 
+Enter First Number: 
+<input type="number" name="number1" /><br><br> 
+Enter Second Number: 
+<input type="number" name="number2" /><br><br> 
+<input type="submit" name="submit" value="sum and product"> 
 </form>
 <?php
 //function definition
@@ -13,17 +13,25 @@ Enter Second Number:
 //and return the sum of them
 function addTwoNumbers(int $x, int $y)
 {
-return $x + $y;
+ return $x + $y;
 }
-?>
-<?php
-if(isset($_POST['submit']))
+function multiplyTwoNumbers(int $x, int $y)
 {
-$number1 = $_POST['number1'];
-$number2 = $_POST['number2'];
-//calling the function and printing the result
-echo " sum of 10 and 25 : " . addTwoNumbers($number1 , $number2);
+ return $x * $y;
 }
-?>
-</body>
+?> 
+<?php 
+ if(isset($_POST['submit'])) 
+ { 
+ $number1 = $_POST['number1']; 
+ $number2 = $_POST['number2']; 
+//calling the function and printing the result
+echo " Sum of $number1 and $number2 : " . addTwoNumbers($number1 , 
+$number2);
+echo "<br>";
+echo " Product of $number1 and $number2 : " . multiplyTwoNumbers($number1 , 
+$number2); 
+} 
+?> 
+</body> 
 </html>
